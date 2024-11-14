6c0e2403fa41678b683bcf8fd98c69b4963ea0c8
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-13 21:09:39 -0800

    Rename foodID to mealName and MealPage to MealDetailsPage

--- src/app/meals/[mealName]/page.js
+++ src/app/meals/[mealName]/page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function MealPage({ params }) {
+export default function MealDetailsPage({ params }) {
     return (
         <main>
-            <h1>Meal page for {params.foodID}</h1>
+            <h1>Meal page for {params.mealName}</h1>
             <p>
                 <Link href="/meals">Go back to meals</Link>
             </p>
