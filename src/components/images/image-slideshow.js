568c31fbf3499371c27b5ed8ce22d0053732ac1a
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-16 22:22:01 -0800

    Import additional images for the slideshow, provide alt captions, and format the component file using prettier

--- src/components/images/image-slideshow.js
+++ src/components/images/image-slideshow.js
@@ -4,20 +4,34 @@ import Image from "next/image";
 
 import styles from "@/styles/image-slideshow.module.css";
 
-import burgerImg from '@/assets/burger.jpg'
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
 
 const images = [
     { image: burgerImg, alt: "A delicious, juicy burger" },
-]
+    { image: curryImg, alt: "A delicious, spicy curry" },
+    { image: dumplingsImg, alt: "Steamed dumplings" },
+    { image: macncheeseImg, alt: "Mac and cheese" },
+    { image: pizzaImg, alt: "A delicious pizza" },
+    { image: schnitzelImg, alt: "A delicious schnitzel" },
+    { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
 
 export default function ImageSlideshow() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+            setCurrentImageIndex(
+                (prevIndex) => (prevIndex + 1) % images.length,
+            );
         }, 5000);
-        
+
         return () => clearInterval(interval);
     }, []);
 
@@ -32,5 +46,5 @@ export default function ImageSlideshow() {
                 />
             ))}
         </div>
-    )
+    );
 }
