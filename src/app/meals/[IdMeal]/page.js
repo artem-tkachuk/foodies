0f24b1089529d9cfb3f2ab97c6f2cf0318168930
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-17 18:03:20 -0800

    Fix meal identifier prop to conform with database schema

--- src/app/meals/[IdMeal]/page.js
+++ src/app/meals/[IdMeal]/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function MealDetailsPage({ params }) {
     return (
         <main>
-            <h1>Meal page for {params.mealName}</h1>
+            <h1>Meal page for {params.IdMeal}</h1>
             <p>
                 <Link href="/meals">Go back to meals</Link>
             </p>
--- src/components/meals/meal-item.js
+++ src/components/meals/meal-item.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import classes from "@/styles/meals/meal-item.module.css";
 
-export default function MealItem({ title, mealName, image, summary, creator }) {
+export default function MealItem({ title, slug, image, summary, creator }) {
     return (
         <article className={classes.meal}>
             <header>
@@ -18,7 +18,7 @@ export default function MealItem({ title, mealName, image, summary, creator }) {
             <div className={classes.content}>
                 <p className={classes.summary}>{summary}</p>
                 <div className={classes.actions}>
-                    <Link href={`/meals/${mealName}`}>View Details</Link>
+                    <Link href={`/meals/${slug}`}>View Details</Link>
                 </div>
             </div>
         </article>
