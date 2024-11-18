05cbda1b03453d97e71c15584d985216e315c7a0
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-17 19:45:38 -0800

    Create an error page for loading meals and trigger meals loading error on the server

---
+++ src/app/meals/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+export default function MealsErrorPage() {
+    return (
+        <main className="error">
+            <h1>An error occurred!</h1>
+            <p>
+                Failed to fetch meal data. PLease try again later!
+            </p>
+        </main>
+    );
+}
--- src/lib/meals.js
+++ src/lib/meals.js
@@ -4,5 +4,7 @@ const db = sql("meals.db");
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 10000)); // Simulate a delay
+
+    // throw new Error("Loading meals failed... oh no!!!!"); // To trigger an example of how to handle an errors
     return db.prepare("SELECT * FROM meals").all();
 }
