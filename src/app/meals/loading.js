b80883cba795055bc629b61deda50aa209d787d7
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-17 18:19:07 -0800

    Inject longer delay and add a loading page to handle it

---
+++ src/app/meals/loading.js
@@ -0,0 +1,5 @@
+import styles from "@/styles/loading/loading.module.css";
+
+export default function MealsLoadingPage() {
+    return <p className={styles.loading}>Fetching meals...</p>;
+}
--- src/lib/meals.js
+++ src/lib/meals.js
@@ -3,6 +3,6 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
+    await new Promise((resolve) => setTimeout(resolve, 10000)); // Simulate a delay
     return db.prepare("SELECT * FROM meals").all();
 }
