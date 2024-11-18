3dfe091cc0b20672f373eedc9ca601decb3d8ba4
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-17 19:51:20 -0800

    Create a not-found page for all invalid URL on the website

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFoundPage() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>
+                Unfortunately, the page or resource you are looking for does not
+                exist.
+            </p>
+        </main>
+    );
+}
