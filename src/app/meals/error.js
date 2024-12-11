7dbe9985c85524509d4cba07a7a1b402c43856b6
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-10 22:40:58 -0800

    Fix a typo and formatting

--- src/app/meals/error.js
+++ src/app/meals/error.js
@@ -1,12 +1,10 @@
-'use client';
+"use client";
 
 export default function MealsErrorPage() {
     return (
         <main className="error">
             <h1>An error occurred!</h1>
-            <p>
-                Failed to fetch meal data. PLease try again later!
-            </p>
+            <p>Failed to fetch meal data. PLease try again later!</p>
         </main>
     );
 }
--- src/lib/meals.js
+++ src/lib/meals.js
@@ -5,6 +5,6 @@ const db = sql("meals.db");
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 10000)); // Simulate a delay
 
-    // throw new Error("Loading meals failed... oh no!!!!"); // To trigger an example of how to handle an errors
+    // throw new Error("Loading meals failed... oh no!!!!"); // To trigger an example of how to handle an error
     return db.prepare("SELECT * FROM meals").all();
 }
