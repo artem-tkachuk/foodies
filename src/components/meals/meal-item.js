7a6e0be45caebeff6f69c76cdee6505877171890
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-22 23:59:46 -0800

    Clarifying comment

--- src/components/meals/meal-item.js
+++ src/components/meals/meal-item.js
@@ -8,7 +8,9 @@ export default function MealItem({ title, slug, image, summary, creator }) {
         <article className={classes.meal}>
             <header>
                 <div className={classes.image}>
-                    <Image src={image} alt={title} fill />
+                    {/* fill is an alternative to width and height when we don't know the size of the image in advance, e.g. when fetching from an API */}
+                    {/* If you know the size of the image in advance, you should use width and height */}
+                    <Image src={image} alt={title} fill /> 
                 </div>
                 <div className={classes.headerText}>
                     <h2>{title}</h2>
