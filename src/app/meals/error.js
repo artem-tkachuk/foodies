56f9af82b2dae2ca3398d0dec52c158a4ad0191d
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-24 00:15:02 -0800

    Typo + clarification

--- src/app/meals/error.js
+++ src/app/meals/error.js
@@ -1,10 +1,11 @@
 "use client";
 
+// in Error({error}), the exact error message will be hidden to avoid leaking sensitive information to users
 export default function MealsErrorPage() {
     return (
         <main className="error">
             <h1>An error occurred!</h1>
-            <p>Failed to fetch meal data. PLease try again later!</p>
+            <p>Failed to fetch meal data. Please try again later!</p>
         </main>
     );
 }
