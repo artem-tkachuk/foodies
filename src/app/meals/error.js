ef57344cdbabf9f278f7be36b71cbdaafe721ef9
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-24 00:17:05 -0800

    Outputting error message from the database request in the UI using component prop to Error component

--- src/app/meals/error.js
+++ src/app/meals/error.js
@@ -1,11 +1,12 @@
 "use client";
 
 // in Error({error}), the exact error message will be hidden to avoid leaking sensitive information to users
-export default function MealsErrorPage() {
+export default function MealsErrorPage({ error }) {
     return (
         <main className="error">
             <h1>An error occurred!</h1>
             <p>Failed to fetch meal data. Please try again later!</p>
+            <p>{error.message}</p>
         </main>
     );
 }
