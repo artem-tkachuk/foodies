0a40bb876ed122ac736dfeda88b98d89920a299f
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-24 18:13:10 -0800

    Add a function to fetch a single meal from the db that protects against SQL injection attacks

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -8,5 +8,15 @@ export async function getMeals() {
 
     // throw new Error("Loading meals failed... oh no!!!!"); // To trigger an example of how to handle an error
     // .run() is used when changing data, but .all() is used when fetching data and we want to get back all rows. .get() is used for a single row
-    return db.prepare("SELECT * FROM meals").all(); //
+    return db.prepare("SELECT * FROM meals").all(); 
+}
+
+// export async function getMeal(idMeal) {
+export function getMeal(idMeal) {
+    // Simulate a delay
+    // await new Promise((resolve) => setTimeout(resolve, 10000)); 
+
+    // not using ? opens us up to SQL injection attacks
+    // sqlite3 will protect us from this
+    return db.prepare("SELECT * FROM meals WHERE slug = ?").get(idMeal); // slug is the column name in the meals table
 }
