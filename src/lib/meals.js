3742fd1e0541d513af83cc473a30594247e0ca58
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-24 18:11:58 -0800

    Remove delay

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -3,9 +3,10 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-    await new Promise((resolve) => setTimeout(resolve, 10000)); // Simulate a delay
+    // Simulate a delay
+    // await new Promise((resolve) => setTimeout(resolve, 2000)); 
 
     // throw new Error("Loading meals failed... oh no!!!!"); // To trigger an example of how to handle an error
     // .run() is used when changing data, but .all() is used when fetching data and we want to get back all rows. .get() is used for a single row
-    return db.prepare("SELECT * FROM meals").all(); // 
+    return db.prepare("SELECT * FROM meals").all(); //
 }
