78d15889d4dea30e6022f3d098067ff92e21446b
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-24 18:15:47 -0800

    Create not found page for when looking up a specific meal

---
+++ src/app/meals/[IdMeal]/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>
+                Unfortunately, we could not find the requested meal data.
+            </p>
+        </main>
+    );
+}
