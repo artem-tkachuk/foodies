b832d64280401c34eec6e5415ef2725e3786a0c7
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-25 22:58:30 -0800

    Improve ImagePicker component to support image selection and preview functionality

--- src/components/meals/image-picker.js
+++ src/components/meals/image-picker.js
@@ -1,23 +1,55 @@
 "use client";
 
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 import styles from "@/styles/meals/image-picker.module.css";
+import Image from "next/image";
 
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState(null);
+    
     const imageInputRef = useRef();
     
     function handlePickerClick() {
         imageInputRef.current.click();
     }
 
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        setPickedImage(file);
+
+        // Convert to Data URL – a value that can be used as an image source (input for an img tag)
+        const fileReader = new FileReader();
+        // When the file is read, the onload function is called
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
 
     return (
         <>
             <div className={styles.picker}>
                 <label htmlFor={name}>{label}</label>
                 <div className={styles.controls}>
+                    <div className={styles.preview}>
+                        {!pickedImage ? 
+                            <p>No image picked yet</p> 
+                            : 
+                            <Image 
+                                src={pickedImage} 
+                                alt="Image selected by the user for their meal" 
+                                fill 
+                            />
+                        }
+                    </div>
                     {/* 
                         Because of the "display: none" in the stylesheet, the input is hidden.
                         But it's still part of the DOM!
@@ -29,6 +61,8 @@ export default function ImagePicker({ label, name }) {
                         name={name}
                         accept="image/png, image/jpeg"
                         ref={imageInputRef}
+                        // multiple={true} { /* If we want to allow the user to pick multiple images, but we don't need that here */ }
+                        onChange={handleImageChange}
                     />
                     {/* 
                         If we don't set the button type to "button", it will default to "submit" 
