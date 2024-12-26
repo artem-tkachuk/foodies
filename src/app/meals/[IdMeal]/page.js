8c0ca87262c3217ce3589d482c57d6ca2bbcef0b
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-25 16:29:49 -0800

    Remove unused imports and clean up MealDetailsPage component structure

--- src/app/meals/[IdMeal]/page.js
+++ src/app/meals/[IdMeal]/page.js
@@ -1,9 +1,6 @@
 import { notFound } from "next/navigation";
 // import Link from "next/link";
 import Image from "next/image";
-import { Suspense } from "react";
-
-import LoadingMealDetails from "./loading";
 
 import { getMeal } from "@/lib/meals";
 
