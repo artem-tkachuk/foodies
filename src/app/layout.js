b0b863170274acb597b172a9dd5603f335307b2c
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-12-30 22:44:05 -0800

    Add custom metadata to meals page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import MainHeader from "@/components/main-header/main-header";
 
 import "@/styles/globals.css";
 
+// Next.js looks for exported constants or variables called metadata for all pages and layouts
 export const metadata = {
     title: "NextLevel Food",
     description: "Delicious meals, shared by a food-loving community.",
--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -7,6 +7,11 @@ import { getMeals } from "@/lib/meals";
 
 import styles from "@/styles/meals/meals.module.css";
 
+export const metadata = {
+    title: "All meals",
+    description: "Browse the delicious meals shared by our vibrant community!",
+};
+
 async function Meals() {
     // [FIXED] This is not logged anymore in PROD build on every run of this component because these pages are pregenerated and cached (by default)
     console.log("Fetching meals..."); 
